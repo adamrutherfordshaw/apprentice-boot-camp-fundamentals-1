a1683f315e275ac0e39b9ee8e5490fe8796c7532
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-03-15 15:59:02 +0000

    refactor cards.js

--- exercises/javascript/cards/cards.js
+++ exercises/javascript/cards/cards.js
@@ -1,81 +1,39 @@
-// to execute the runnable code in this file, use the command
-// `node cards/cards.js` from the command line positioned at
-// the project's root directory.
-
-let Cards = class Cards {
-};
-
-Cards.prototype.getCards = function getCards() {
-  let result = new Array(52);
-  let deck = createArray(52, 2); // There is no multidimensional array constructor in JavaScript, this creates an a
-
-  for (let suit = 0; suit < 4; suit++) {
-    for (let faceValue = 0; faceValue < 13; faceValue++) {
-      deck[suit * 13 + faceValue] = [suit, faceValue];
-    }
+class Deck {
+  constructor() {
+    this.cards = [];
+    this.suits = ["clubs", "diamonds", "hearts", "spades"];
   }
 
-  let cardNumber = 0;
-  for (let card of deck) {
-    let faceValueName;
-    switch (card[1]) {
-      case 0:
-        faceValueName = "ace";
-        break;
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-        faceValueName = (card[1] + 1).toString();
-        break;
-      case 10:
-        faceValueName = "jack";
-        break;
-      case 11:
-        faceValueName = "queen";
-        break;
-      case 12:
-        faceValueName = "king";
-        break;
-      default:
-        throw new Error("Something went wrong " + card[1] + " is not a valid faceValue!");
-    }
-
-    let suitName;
-    switch (card[0]) {
-      case 0: suitName = "clubs"; break;
-      case 1: suitName = "diamonds"; break;
-      case 2: suitName = "hearts"; break;
-      case 3: suitName = "spades"; break;
-      default: throw new Error("Something went wrong " + card[0] + " is not a valid suitName!");
-    }
-
-    result[cardNumber] = faceValueName + " of " + suitName;
-    cardNumber++;
+  printCards() {
+    this.createCards();
+    return this.cards;
   }
 
-  return result;
-};
-
-let createArray = function (a, b) {
-  let x = new Array(a);
-  for (let i = 0; i < x.length; i++) {
-    x[i] = new Array(b);
+  createCards() {
+    for (this.suit of this.suits) {
+      for (let val = 1; val <= 13; val++) {
+        this.cards.push(new Card(val, this.suit).formatCard());
+      }
+    }
   }
-  return x;
-};
+}
 
-let cards = new Cards();
-let deckInOrder = cards.getCards();
-for (const card of deckInOrder) {
-  console.log(card);
+class Card {
+  constructor(value, suit) {
+    this.value = value;
+    this.suit = suit;
+  }
+
+  formatCard() {
+    if (this.value === 1) this.value = "ace";
+    else if (this.value === 11) this.value = "jack";
+    else if (this.value === 12) this.value = "queen";
+    else if (this.value === 13) this.value = "king";
+
+    return this.value + " of " + this.suit;
+  }
 }
 
 module.exports = {
-  Cards: Cards
+  Deck: Deck,
 };
--- exercises/javascript/jest-test/cards/cards.test.js
+++ exercises/javascript/jest-test/cards/cards.test.js
@@ -1,9 +1,9 @@
-const { Cards } = require('../../cards/cards.js');
+const { Deck } = require("../../cards/cards.js");
 
-describe('getCards', function () {
-  test('get deck in order, return 52 cards correctly formatted', function () {
-    let cards = new Cards();
-    expect(cards.getCards()).toStrictEqual([
+describe("getCards", function () {
+  test("get deck in order, return 52 cards correctly formatted", function () {
+    let deck = new Deck();
+    expect(deck.printCards()).toStrictEqual([
       "ace of clubs",
       "2 of clubs",
       "3 of clubs",
@@ -55,7 +55,7 @@ describe('getCards', function () {
       "10 of spades",
       "jack of spades",
       "queen of spades",
-      "king of spades"
+      "king of spades",
     ]);
   });
 });
