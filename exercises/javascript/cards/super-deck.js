eef9745a0f1668e6c0225c1b405dc22ea548b201
Adam Rutherford-Shaw <adam.rutherfordshaw@autotrader.co.uk>
2021-03-16 16:17:17 +0000

    add superdeck to snap

---
+++ exercises/javascript/cards/super-deck.js
@@ -0,0 +1,12 @@
+const { Deck } = require("./deck.js");
+
+class SuperDeck extends Deck {
+  constructor(animalDeck, playingDeck) {
+    super();
+    this.cards = [...animalDeck.cards, ...playingDeck.cards];
+  }
+}
+
+module.exports = {
+  SuperDeck: SuperDeck,
+};
